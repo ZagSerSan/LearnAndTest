9c8a1e23cbf114c92a6d908b368b4184d5d401ee
ZagSerSan <zagsersan2017@tutamail.com>
2020-07-26 11:40:56 +0300

    start string learning

---
+++ app.js
@@ -0,0 +1,26 @@
+// const name = 'Serhii'
+// const age = 22
+
+// function getAge() {
+// 	return age
+// }
+
+// const output = `Привет, меня зовут ${name}, а мой возраст ${age > 21 ? 'A' : 'B'} лет.`
+
+// const output = `
+// 	<div class="block">это блок див</div>
+// 	<p>Это блок п</p>
+// `
+
+// console.log(output)
+
+const name = 'Serhii'
+
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.charAt(2))
+console.log(name.indexOf('hii'))
+
+
+
