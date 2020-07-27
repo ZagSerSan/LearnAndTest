fe7c6e42954261268ec036ce397f6a7f6638c22d
ZagSerSan <zagsersan2017@tutamail.com>
2020-07-27 17:22:12 +0300

    some learn js

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-// const name = 'Serhii'
-// const age = 22
+// ФУНКЦИЯ
 
-// function getAge() {
-// 	return age
-// }
+function retName (name) {
+	console.log('Привет - ', name)
+}
 
-// const output = `Привет, меня зовут ${name}, а мой возраст ${age > 21 ? 'A' : 'B'} лет.`
-
-// const output = `
-// 	<div class="block">это блок див</div>
-// 	<p>Это блок п</p>
-// `
-
-// console.log(output)
-
-const name = 'Serhii'
-
-console.log(name.length)
-console.log(name.toUpperCase())
-console.log(name.toLowerCase())
-console.log(name.charAt(2))
-console.log(name.indexOf('hii'))
-
-
-
+retName('Лена')
