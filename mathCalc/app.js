ea8a089cfba5e27e3fa1c761ad4c8c35cbb1254d
ZagSerSan <zagsersan2017@tutamail.com>
2020-07-28 20:13:57 +0300

    start programming keys

--- mathCalc/app.js
+++ mathCalc/app.js
@@ -1,32 +1,50 @@
-elBooked = document.getElementById('elBooked')
-elFree = document.getElementById('elFree')
-elWrite = document.getElementById('elWrite')
-elCalc = document.getElementById('elCalc')
+let title = document.getElementById('block-title')
 
-let hotel = {
-	name: 'Tula',
-	rooms: 40,
-	booked: 12,
-
-	getBooked: function(){
-		hotel.booked = parseInt(prompt('Введите кол-во занятых номеров'))
-		console.log(hotel.rooms)
-		console.log(hotel.booked)
-		
-		elBooked.textContent = hotel.booked
+let calculator = {
+	input1: function () {
+		if (title.innerText === '0') {
+			title.innerText = ''
+			let temp = title.innerText + '1'
+			title.textContent = temp
+		} else {
+			let temp = title.innerText + '1'
+			title.textContent = temp
+		}
 	},
+	input2: function () {
+			if (title.innerText === '0') {
+				title.innerText = ''
+				let temp = title.innerText + '2'
+				title.textContent = temp
+			} else {
+				let temp = title.innerText + '2'
+				title.textContent = temp
+			}
+		},
 
-	calcFreeRoom: function(){
-		let free = hotel.rooms - hotel.booked
-		console.log(free)
-		elFree.textContent = free
+	del: function () {
+		temp = '0'
+		title.textContent = temp
 	}
 }
 
+let btn1 = document.getElementById('btn1')
+btn1.addEventListener('click', calculator.input1)
+
+let btn2 = document.getElementById('btn2')
+btn2.addEventListener('click', calculator.input2)
+
+let btnCE = document.getElementById('btnCE')
+btnCE.addEventListener('click', calculator.del)
 
 
-elWrite.addEventListener('click', hotel.getBooked)
-elCalc.addEventListener('click', hotel.calcFreeRoom)
+
+
+
+
+
+
+
 
 
 
