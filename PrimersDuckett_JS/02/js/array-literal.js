febebed9a990bf30252a4139f71c2688198b9eb8
ZagSerSan <yan.steys1997@gmail.com>
2022-07-05 12:00:10 +0200

    worked with primers

--- PrimersDuckett_JS/02/js/array-literal.js
+++ PrimersDuckett_JS/02/js/array-literal.js
@@ -1,2 +1,14 @@
-﻿// Создаем массив и присваиваем значения.var colors; colors = ['белый', 'черный', 'пользовательский'];// Отображаем первый элемент массива.var el = document.getElementById('colors');el.textContent = colors[0];/* NOTE: textContent не работает в браузере IE8 и версии ниже
-Вы можете использовать innerHTML, но возникает угроза безопасности сайта, как описано в книге:el.textContent = colors[0];*/
+﻿// Создаем массив и присваиваем значения.
+var colors; 
+colors = ['белый', 'черный', 'пользовательский'];
+
+// Отображаем первый элемент массива.
+var el = document.getElementById('colors');
+el.textContent = colors[0];
+
+/* 
+NOTE: textContent не работает в браузере IE8 и версии ниже
+Вы можете использовать innerHTML, но возникает угроза безопасности сайта, как описано в книге:
+
+el.textContent = colors[0];
+*/
