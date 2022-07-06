9d18dbcec382760e6732bb344cc35e94771027aa
ZagSerSan <yan.steys1997@gmail.com>
2022-07-06 12:04:02 +0200

    imnproving the example

--- PrimersDuckett_JS/02/js/example.js
+++ PrimersDuckett_JS/02/js/example.js
@@ -8,14 +8,15 @@ var welcome = greeting + name + message;
 // Создаем переменные, в которых будет храниться подробная информация о табличке
 var sign = 'Всемирная сеть';
 var tiles = sign.length;
-var subTotal = tiles * 5;
+var letterCost = 5;
+var subTotal = tiles * letterCost;
 var shipping = 7;
 var grandTotal = subTotal + shipping;
 
 // Получаем элемент с идентификатором greeting
-var el = document.getElementById('greeting');
+var elWelcome = document.getElementById('greeting');
 // Заменяем содержимое элемента на персонализированное приветственное сообщение
-el.textContent = welcome;
+elWelcome.textContent = welcome;
 
 // Получаем элемент с идентификатором userSign и обновляем его содержимое
 var elSign = document.getElementById('userSign');
@@ -27,15 +28,15 @@ elTiles.textContent = tiles;
 
 // Получаем элемент с идентификатором subTotal и обновляем его содержимое
 var elSubTotal = document.getElementById('subTotal');
-elSubTotal.textContent = subTotal + ' ₽';
+elSubTotal.textContent = subTotal + ' р';
 
 // Получаем элемент с идентификатором shipping и обновляем его содержимое
 var elShipping = document.getElementById('shipping');
-elShipping.textContent = shipping + ' ₽';
+elShipping.textContent = shipping + ' р';
 
 // Получаем элемент с идентификатором grandTotal и обновляем его содержимое
 var elGrandTotal = document.getElementById('grandTotal');
-elGrandTotal.textContent = grandTotal + ' ₽';
+elGrandTotal.textContent = grandTotal + ' р';
 
 /* 
 ПРИМЕЧАНИЕ: textContent не работает в браузере IE8 и версии ниже
