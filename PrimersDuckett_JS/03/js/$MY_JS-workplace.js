2096a6ada31e50c4c8d4e4786c47eea4cff687e4
ZagSerSan <yan.steys1997@gmail.com>
2022-07-07 10:27:37 +0200

    add comments

--- PrimersDuckett_JS/03/js/$MY_JS-workplace.js
+++ PrimersDuckett_JS/03/js/$MY_JS-workplace.js
@@ -1,9 +1,11 @@
- 
+// Объявление функции
 function getArea(width, heigth) {
   return width * heigth;
 }
 
+// Вызов функции
 var msg = getArea(10, 12);
 
+// Вывод данных
 var el = document.getElementById('message');
 el.textContent = msg;
