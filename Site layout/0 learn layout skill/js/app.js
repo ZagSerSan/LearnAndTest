59b3db56ef2e16f3573e268ef30996c45add886b
ZagSerSan <yan.steys1997@gmail.com>
2022-09-21 09:10:47 +0200

    end testind

--- Site layout/0 learn layout skill/js/app.js
+++ Site layout/0 learn layout skill/js/app.js
@@ -1,4 +1,6 @@
-var var1 = (document.getElementById('a_data')).dataset.link;
+var var1 = document.getElementById('p_data');
+var var2 = var1.getAttribute("data-index");
 
 console.log(var1);
+console.log(var2);
 
