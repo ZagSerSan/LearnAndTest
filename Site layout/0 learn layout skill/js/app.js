0a4fc0c9944b25235cff9004ad80cb66998eb305
ZagSerSan <yan.steys1997@gmail.com>
2022-09-21 08:59:11 +0200

    css by data-*

--- Site layout/0 learn layout skill/js/app.js
+++ Site layout/0 learn layout skill/js/app.js
@@ -1,11 +1,4 @@
-var var1 = document.getElementById('a_data');
-var var2 = var1.getAttribute('data-link');
+var var1 = (document.getElementById('a_data')).dataset.link;
 
-var1.onclick = function (even) {
-   even.preventDefault();
-
-   var1.classList.toggle('active')
-   console.log('class toggled');
-}
-
+console.log(var1);
 
