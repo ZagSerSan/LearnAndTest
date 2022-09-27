2753583a00821a75d1ce4c8f75793759f801430d
ZagSerSan <yan.steys1997@gmail.com>
2022-09-27 07:50:38 +0200

    noname

--- Site layout/0 learn layout skill/js/app.js
+++ Site layout/0 learn layout skill/js/app.js
@@ -5,11 +5,32 @@ var elData = el.dataset.link;
 el.onclick = function() {
   this.classList.toggle('active');
 }
-console.log(elData);*/
+
+var linkSelector = document.getElementById('link');
+function activeLink(linkSelector) {
+  const links = document.querySelectorAll(linkSelector);
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      links.forEach((item) => {
+        item.classlist.remove(linkSelector);
+      });
+      link.classlist.add(linkSelector);
+    });
+  });
+}
+activeLink(linkSelector);
+*/
+$(function () {
+  
 let linkEl = $('#link');
 
 $('[data-link]').on('click', function (even) {
   even.preventDefault();
   var $this = $(this);
   $this.toggleClass('active');
+});
+
+
+
 });
