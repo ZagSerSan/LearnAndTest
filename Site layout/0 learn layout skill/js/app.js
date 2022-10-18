a6a75e56ca75a7e821cbc16442e991de4766d05a
ZagSerSan <yan.steys1997@gmail.com>
2022-10-18 14:25:28 +0200

    область видимости

--- Site layout/0 learn layout skill/js/app.js
+++ Site layout/0 learn layout skill/js/app.js
@@ -2,17 +2,17 @@
 var log1 = document.getElementById('text1JS');
 var log2 = document.getElementById('text2JS');
 
-var getArea = (function () {
+var width = 4;
+
+function getArea () {
   var width = 3;
   var height = 5;
-  var size = width * height;
-  console.log(size);
+  var size = [width, height];
   return size
-}())
+}
 
-
-// log1.textContent = out1;
-// log2.textContent = getArea(3,3,2)[1];
+log1.textContent = getArea()[0];
+log2.textContent = width;
 
 
 /* КНОПКИ ССЫЛКИ (data-*)
