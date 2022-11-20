83bc597182e795ace51910f011e8d817640cae1b
ZagSerSan <yan.steys1997@gmail.com>
2022-11-20 10:41:31 +0100

    cleaning and preparing the file for new project

--- Site layout/0 learn layout skill/js/app.js
+++ Site layout/0 learn layout skill/js/app.js
@@ -1,38 +1,4 @@
 
-function activeLink (linkSelector, classActive) {
-  const links = document.querySelectorAll(linkSelector);
-  links.forEach(linksElement1 => {
-    linksElement1.addEventListener('click', (e) =>{
-      e.preventDefault();
-      links.forEach(linksElement2 => {
-        linksElement2.classList.remove('active');
-      });
-      linksElement1.classList.add('active')
-    });
-  });
-}
-activeLink('#link','active');
 
-
-
-// scroll
-
-// const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-const anchors = document.querySelectorAll('a[href^="#"]');
-
-anchors.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-    var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.scrollY;
-    window.scrollTo({
-      top: coordY,
-      behavior: "smooth"
-    });
-  
-  });
-});
-
-// console.log(anchors);
 
 
