2182e10974f74bc934ed040866d1a57f31dc99bb
ZagSerSan <yan.steys1997@gmail.com>
2023-01-02 11:32:47 +0100

    todo 1 is done

--- Site layout/0 learn layout skill/js/app.js
+++ Site layout/0 learn layout skill/js/app.js
@@ -1,15 +1,35 @@
 //TODO_TEMP ==================================================
 /* 
-   Пропорциональное изменение высоты изображения относительно его ширины.
+   1 непоказ слайдера первого +
+   2 Пропорциональное изменение высоты изображения относительно его ширины.
 */
 
+let item = document.querySelector('.item');
+let modal = document.querySelector('.modal');
+let close = document.querySelector('.close');
+
+setTimeout(() => {
+   modal.style.display = 'none';
+   modal.style.opacity = '1';
+}, 100);
 
 
+item.addEventListener('click', () => {
+   modal.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+   modal.style.display = 'none';
+});
+
+$(function() {
+
+   $('.slider').slick({
+      // arrows: false
+   });
 
 
-
-
-
+});
 
 
 
