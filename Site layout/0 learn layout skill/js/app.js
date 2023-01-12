f4185075491ff4e8045993e989babc3f7b448678
ZagSerSan <yan.steys1997@gmail.com>
2023-01-12 10:45:41 +0100

    restoring the slider for testing and debugging

--- Site layout/0 learn layout skill/js/app.js
+++ Site layout/0 learn layout skill/js/app.js
@@ -4,7 +4,31 @@
 */
 
 
+let item = document.querySelector('.item');
+let modal = document.querySelector('.modal');
+let close = document.querySelector('.close');
 
+setTimeout(() => {
+   modal.style.display = 'none';
+   modal.style.opacity = '1';
+}, 100);
+
+
+item.addEventListener('click', () => {
+   modal.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+   modal.style.display = 'none';
+});
+
+$(function() {
+
+   $('.slider').slick({
+      // arrows: false
+   });
+
+});
 
 
 
