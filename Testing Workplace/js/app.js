fc87028fc0c9a0595c236a7a7c69b23600a6d2b1
ZagSerSan <yan.steys1997@gmail.com>
2023-01-24 15:37:49 +0100

    file correction

---
+++ Testing Workplace/js/app.js
@@ -0,0 +1,36 @@
+//TODO_TEMP ==================================================
+/* 
+   1 Пропорциональное изменение высоты изображения относительно его ширины.
+*/
+
+let item = document.querySelector('.item');
+let modal = document.querySelector('.modal');
+let modal_bg = document.querySelector('.modal-bg');
+let btn_close = document.querySelector('.close');
+
+item.addEventListener('click', () => {
+  $(function () {
+    $('.slider').slick('setPosition');
+  });
+  modal.classList.add('active');
+
+  modal_bg.classList.add('active');
+  setTimeout(() => {
+    modal_bg.classList.add('bg');
+  }, 10);
+});
+
+btn_close.addEventListener('click', () => {
+  modal.classList.remove('active');
+
+  modal_bg.classList.remove('bg');
+  setTimeout(() => {
+    modal_bg.classList.remove('active');
+  }, 100);
+});
+
+$(function () {
+  $('.slider').slick({
+    // arrows: false
+  });
+});
