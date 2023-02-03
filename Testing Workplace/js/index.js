4fdb39f8b0e39110091c7ce757373bcd5404241e
ZagSerSan <yan.steys1997@gmail.com>
2023-02-03 18:07:30 +0100

    Задание #1

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,1 +1,13 @@
 //TODO ========= 1й Модуль - 5.Работа со строками ==============
+
+console.log('-----------------Задание #1-------------------')
+
+const myName = 'Сегрей';
+const programmingLanguage = 'JavaScript';
+const courseCreatorName = 'Владилена Минина';
+const reasonText = 'хочу изменить свою жизнь';
+const numberOfMonth = 12;
+
+let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев. Я уверен, что пройду данный курс до конца!`;
+
+console.log(myInfoText);
