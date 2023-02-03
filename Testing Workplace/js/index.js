28ab4239a22ae6adf1b2f8d8142eed027269f41f
ZagSerSan <yan.steys1997@gmail.com>
2023-02-03 18:23:13 +0100

    Задание #2

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,6 +1,6 @@
 //TODO ========= 1й Модуль - 5.Работа со строками ==============
 
-console.log('-----------------Задание #1-------------------')
+console.log('----------------- Задание #1 -------------------')
 
 const myName = 'Сегрей';
 const programmingLanguage = 'JavaScript';
@@ -9,5 +9,24 @@ const reasonText = 'хочу изменить свою жизнь';
 const numberOfMonth = 12;
 
 let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев. Я уверен, что пройду данный курс до конца!`;
-
 console.log(myInfoText);
+
+console.log('----------------- Задание #2 -------------------')
+
+myInfoText = myInfoText.replaceAll('JavaScript', 'javascript');
+myInfoText = myInfoText.replaceAll('курс', 'КУРС');
+console.log(myInfoText);
+console.log(myInfoText.length);
+console.log(myInfoText[0], myInfoText[(myInfoText.length - 1)]);
+
+
+
+
+
+
+
+
+
+
+
+
