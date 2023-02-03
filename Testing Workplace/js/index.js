25d24ad6f67fada702f273774877a12fe58f342b
ZagSerSan <yan.steys1997@gmail.com>
2023-02-03 18:30:11 +0100

    Задание #3

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -11,6 +11,7 @@ const numberOfMonth = 12;
 let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев. Я уверен, что пройду данный курс до конца!`;
 console.log(myInfoText);
 
+
 console.log('----------------- Задание #2 -------------------')
 
 myInfoText = myInfoText.replaceAll('JavaScript', 'javascript');
@@ -20,10 +21,12 @@ console.log(myInfoText.length);
 console.log(myInfoText[0], myInfoText[(myInfoText.length - 1)]);
 
 
+console.log('----------------- Задание #3 -------------------')
 
-
-
-
+let userName = prompt('Как вас зовут?');
+userName = (userName.toUpperCase()).trim();
+console.log(userName);
+alert(`Вас зовут ${userName}.`)
 
 
 
