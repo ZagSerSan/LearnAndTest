d1aff06b37d64b3a94c9cb72b2a6e13fd86fea67
ZagSerSan <yan.steys1997@gmail.com>
2023-02-09 17:08:43 +0100

    Задание #1

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -4,6 +4,27 @@
 
 console.log('----------------- Задание #1 -------------------')
 
+function getName1(name) {
+   return `Имя равно ${name}`;
+}
+
+const getName2 =  function(name) {
+   return `Имя равно ${name}`;
+}
+
+const getName3 = name => {
+   return `Имя равно ${name}`;
+}
+
+console.log('getName1', getName1('Serhii'));
+console.log('getName2', getName2('Serhii'));
+console.log('getName3', getName3('Serhii'));
+*/
+
+console.log('----------------- Задание #2 -------------------')
+
+
+
 
 
 /*
