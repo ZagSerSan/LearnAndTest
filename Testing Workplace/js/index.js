342798cb2e024411fa642966879c451946f0e9be
ZagSerSan <yan.steys1997@gmail.com>
2023-02-09 18:25:27 +0100

    Задание #2 (разбор)

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -19,12 +19,16 @@ const getName3 = name => {
 console.log('getName1', getName1('Serhii'));
 console.log('getName2', getName2('Serhii'));
 console.log('getName3', getName3('Serhii'));
+*/
+
 
 console.log('----------------- Задание #2 -------------------')
 
-function getSumOfNumbers(number, type) {
+function getSumOfNumbers(callback, number, type) {
    let sum = 0;
 
+   callback(`Sum ${number} by ${type}:`);
+  // logNum(`Sum ${number} by ${type}:`)
    for (let i = 0; i <= number; i++) {
       // определение type
       if (type === '') sum += i;
@@ -40,21 +44,21 @@ function getSumOfNumbers(number, type) {
    return sum;
 }
 
-console.log(getSumOfNumbers(10, 'odd')); // 25
-console.log(getSumOfNumbers(10, 'even')); // 30
-console.log(getSumOfNumbers(10, '')); // 55
+const logNum = msg => console.log(msg);
 
-console.log(getSumOfNumbers(20, 'odd')); // 100
-console.log(getSumOfNumbers(20, 'even')); // 110
-console.log(getSumOfNumbers(20, '')); // 210
-*/
-
-console.log('----------------- Задание #3 -------------------')
+console.log(getSumOfNumbers(logNum, 10, 'odd')); // 25
+console.log(getSumOfNumbers(logNum, 10, 'even')); // 30
+console.log(getSumOfNumbers(logNum, 10, '')); // 55
 
 
-/*
-   #1 https://codepen.io/ZagSerSan/pen/vYaqmpd
-   #2 https://codepen.io/ZagSerSan/pen/WNKqjdW
-   #3 
-   #4 
-*/
+// console.log('----------------- Задание #3 -------------------')
+
+
+
+
+
+
+
+
+
+
