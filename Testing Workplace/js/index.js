b6e551f7cae9e16e520052d5e10ac441bed7f2f0
ZagSerSan <yan.steys1997@gmail.com>
2023-02-09 17:55:36 +0100

    Задание #2

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -19,17 +19,42 @@ const getName3 = name => {
 console.log('getName1', getName1('Serhii'));
 console.log('getName2', getName2('Serhii'));
 console.log('getName3', getName3('Serhii'));
-*/
 
 console.log('----------------- Задание #2 -------------------')
 
+function getSumOfNumbers(number, type) {
+   let sum = 0;
 
+   for (let i = 0; i <= number; i++) {
+      // определение type
+      if (type === '') sum += i;
+      if (type === 'even') {
+         sum += i;
+         i++;
+      }
+      if (type === 'odd' && i !== number) {
+         sum += (i+1);
+         i++;
+      }
+   }
+   return sum;
+}
 
+console.log(getSumOfNumbers(10, 'odd')); // 25
+console.log(getSumOfNumbers(10, 'even')); // 30
+console.log(getSumOfNumbers(10, '')); // 55
+
+console.log(getSumOfNumbers(20, 'odd')); // 100
+console.log(getSumOfNumbers(20, 'even')); // 110
+console.log(getSumOfNumbers(20, '')); // 210
+*/
+
+console.log('----------------- Задание #3 -------------------')
 
 
 /*
-   #1 
-   #2 
+   #1 https://codepen.io/ZagSerSan/pen/vYaqmpd
+   #2 https://codepen.io/ZagSerSan/pen/WNKqjdW
    #3 
    #4 
 */
