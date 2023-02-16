30e7b0ac658804fe9fdb825ae4831cb096f086ea
ZagSerSan <yan.steys1997@gmail.com>
2023-02-16 11:57:53 +0100

    Модуль 1. База JavaScript 9. Массивы - задание №1

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -2,3 +2,49 @@
 
 console.log('----------------- Задание #1 -------------------')
 
+const peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария', 'Светлана', 'Артем', 'Глеб'];
+
+function giveParcel() {
+   let userName = peopleWaiting.shift();
+   let length = peopleWaiting.length;
+   if (userName === 'Кирилл') {
+      userName += ', был последним клиентом, который'
+   }
+   alert(`${userName} получил(а) посылку. В очереди осталось ${length} человек.`);
+   console.log(peopleWaiting);
+}
+
+function leaveQueueWithoutParcel() {
+   let userName = peopleWaiting.pop();
+   let length = peopleWaiting.length;
+   alert(`${userName} не получил(а) посылку и ушел(ла) из очереди. В очереди осталось ${length} человек.`);
+   console.log(peopleWaiting);
+}
+
+// 3 первых получили посылки:
+giveParcel();
+giveParcel();
+giveParcel();
+
+// Остальные уходят:
+for (let i = peopleWaiting.length; i > 0; i--) {
+   leaveQueueWithoutParcel()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
