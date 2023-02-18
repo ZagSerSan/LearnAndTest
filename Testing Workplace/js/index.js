db1c0690875c3bb06b121c968dcd58ab8cd264f5
ZagSerSan <yan.steys1997@gmail.com>
2023-02-18 12:05:15 +0100

    доп задание на смекалку

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -2,12 +2,16 @@
 
 console.log('----------------- Задание #1 -------------------')
 
+function filter_list(array) {
+   let newArray = array.filter(item => {
+      return typeof item === 'number';
+   });
+   return newArray;
+}
 
-
-
-
-
-
+console.log(filter_list([1,2,'a','b']));
+console.log(filter_list([1,'a','b',0,15]));
+console.log(filter_list([1,2,'aasf','1','123',123]));
 
 
 
