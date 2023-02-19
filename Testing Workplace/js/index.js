ccc3493f51d9c1832c6db39e205d4bb46bb5a88a
ZagSerSan <yan.steys1997@gmail.com>
2023-02-19 11:51:52 +0100

    1й Модуль - 9. Массивы Задание #3 -> edited

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -2,17 +2,17 @@
 
 console.log('----------------- Задание #1 -------------------')
 
-function filter_list(array) {
-   let newArray = array.filter(item => {
-      return typeof item === 'number';
-   });
-   return newArray;
+const coffees = ['Latte', 'Cappuccino', 'Americano'];
+let coffeeName = prompt('Поиск кофе по названию:', 'Latte, Cappuccino, Americano').trim().toLowerCase(); 
+
+let index = coffees.findIndex(item => {
+   return item.toLowerCase() === coffeeName;
+});
+
+if (index >= 0) {
+   alert(`Держите ваш любимый кофе ${coffees[index]}. Он ${index+1}-й по популярности в нашей кофейне.`)
+} else {
+   alert('К сожалению, такого вида кофе нет в наличии.');
 }
-
-console.log(filter_list([1,2,'a','b']));
-console.log(filter_list([1,'a','b',0,15]));
-console.log(filter_list([1,2,'aasf','1','123',123]));
-
-
 
 
