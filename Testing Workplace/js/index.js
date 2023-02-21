eef38e2a92031c96bbd389249bee63549c084f4b
ZagSerSan <yan.steys1997@gmail.com>
2023-02-21 15:14:42 +0100

    prepar space

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -2,17 +2,25 @@
 
 console.log('----------------- Задание #1 -------------------')
 
-const coffees = ['Latte', 'Cappuccino', 'Americano'];
-let coffeeName = prompt('Поиск кофе по названию:', 'Latte, Cappuccino, Americano').trim().toLowerCase(); 
 
-let index = coffees.findIndex(item => {
-   return item.toLowerCase() === coffeeName;
-});
 
-if (index >= 0) {
-   alert(`Держите ваш любимый кофе ${coffees[index]}. Он ${index+1}-й по популярности в нашей кофейне.`)
-} else {
-   alert('К сожалению, такого вида кофе нет в наличии.');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
