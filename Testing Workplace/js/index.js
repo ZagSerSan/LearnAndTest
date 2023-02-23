4a157916abdf43213669a3160eaea3a0cee4003a
ZagSerSan <yan.steys1997@gmail.com>
2023-02-23 11:12:49 +0100

    preparation space

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,25 +1,5 @@
 //TODO ========= 1й Модуль ==============
 
-function sum(...rest) {
-  let sum = rest.reduce((a, i) => {
-    a += i;
-    return a
-  });
-  return sum;
-}
-
-console.log(sum(1, 2, 3)) // 6
-console.log(sum(2, 2)) // 4
-console.log(sum(10, 15, 249, 653, 846)) // 1773
-
-
-
-
-
-
-
-
-
 
 
 
