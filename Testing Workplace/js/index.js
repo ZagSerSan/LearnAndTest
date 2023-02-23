4be7a06d5705bd8089a153d665ca56635ebead64
ZagSerSan <yan.steys1997@gmail.com>
2023-02-23 10:37:06 +0100

    prep space

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,34 +1,5 @@
 //TODO ========= 1й Модуль ==============
 
-const student = {
-  fullName: 'Максим',
-  experienceInMonths: 12,
-  stack: ['HTML', 'CSS', 'JavaScript', 'React'],
-}
-
-function giveJobToStudent(obj, jobName) {
-  const updatedObject = {
-    ...obj,
-    job: jobName
-  };
-  alert(`Поздравляем! У студента ${updatedObject.fullName} появилась новая работа! Теперь он ${updatedObject.job}.`);
-  return updatedObject;
-}
-
-const updatedStudent = giveJobToStudent(student, 'веб-разработчик');
-console.log(updatedStudent);
-
-// console.log(student);
-
-
-/*
-updatedStudent = {
-    fullName: 'Максим',
-    experienceInMonths: 12,
-    stack: ['HTML', 'CSS', 'JavaScript', 'React'],
-    job: 'веб-разработчик',
-}
-*/
 
 
 
