b73b49743aaf3f89c953df95956fb97dbdcf0777
ZagSerSan <yan.steys1997@gmail.com>
2023-02-23 10:07:59 +0100

    Задание №3

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,13 +1,34 @@
 //TODO ========= 1й Модуль ==============
 
+const student = {
+  name: 'Maxim',
+  programmingLanguage: 'JavaScript',
+}
 
+// get, add, delete
+function handleObject(obj, key, action) {
+  if (action === 'get') {
+    return obj[key];
+  }
+  if (action === 'add') {
+    obj[key] = ''
+    return obj;
+  }
+  if (action === 'delete') {
+    delete obj[key];
+    return obj;
+  } else {
+    return obj;
+  }
+}
 
+// console.log(handleObject(student, 'programmingLanguage', 'get'));
+// console.log(handleObject(student, 'programmingLanguage', 'add'));
+// console.log(handleObject(student, 'programmingLanguage', 'delete'));
+// console.log(handleObject(student, 'programmingLanguage', 'any value'));
 
-
-
-
-
-
+const result = handleObject(student, 'programmingLanguage', 'delete');
+console.log('result:', result); // { name: 'Maxim' }
 
 
 
