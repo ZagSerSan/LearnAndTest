20fff80c0a52b386990dff5b6f61d3379039226c
ZagSerSan <yan.steys1997@gmail.com>
2023-02-27 11:40:28 +0100

    prep space

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,35 +1,5 @@
 //TODO ========= 1й Модуль ==============
 
-function getDateFormat(date, separator) {
-  date = new Date(date);
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-
-  if (day < 10) {
-    day = `0${String(day)}`;
-  }
-  if (month < 10) {
-    month = `0${String(month)}`;
-  }
-
-  let arr = [day, month, year];
-  console.log(arr);
-
-  if (separator === '-') {
-    return arr.join(separator);
-  } else {
-    return arr.join('.');
-  }
-}
-
-// const date = Date.now();
-const date = new Date('Februery 5, 2001');
-const separator = '-';
-
-// console.log(getDateFormat(date));
-console.log(getDateFormat(date,separator));
-
 
 
 
