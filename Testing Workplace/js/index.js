19d01e485d24431bb76cf68b1fd844333b8e2e1a
ZagSerSan <yan.steys1997@gmail.com>
2023-02-27 11:22:10 +0100

    Задание #1

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,8 +1,24 @@
 //TODO ========= 1й Модуль ==============
 
+function getDateFormat(date, separator) {
+  date = new Date(date);
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
 
+  let arr = [day, month, year];
+  
+  if (separator === '-') {
+    return arr.join(separator);
+  } else {
+    return arr.join('.');
+  }
+}
 
+const date = Date.now();
+const separator = '-';
 
+console.log(getDateFormat(date,separator));
 
 
 
