693381c067e924541a51c6f37d3e6f87f2dd35c7
ZagSerSan <yan.steys1997@gmail.com>
2023-02-27 11:38:07 +0100

    corrected code

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -3,7 +3,7 @@
 function getDateFormat(date, separator) {
   date = new Date(date);
   let day = date.getDate();
-  let month = date.getMonth();
+  let month = date.getMonth() + 1;
   let year = date.getFullYear();
 
   if (day < 10) {
