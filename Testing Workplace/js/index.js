d989ddf9e91cd0e9fb50058c3bc3619e900b58ed
ZagSerSan <yan.steys1997@gmail.com>
2023-02-28 09:02:06 +0100

    Задание №3

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,14 +1,16 @@
 //TODO ========= 1й Модуль ==============
 
 
+const date = new Date(Date.now() + 5000)
+const days = 12;
+console.log('date:', date);
+console.log('days:', days);
 
+function addDays(date, days) {
+  return new Date(date.getTime() + days*24*60*60*1000)
+}
 
-
-
-
-
-
-
+console.log('addDays:', addDays(date, days));
 
 
 
