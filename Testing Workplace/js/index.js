fac6fa4238f258e075cc502ac2350b16997dbd86
ZagSerSan <yan.steys1997@gmail.com>
2023-03-01 20:08:06 +0100

    Задание 3

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,7 +1,31 @@
 //TODO ========= 1й Модуль ==============
 
+const footballer = {
+  fullName: 'Cristiano Ronaldo',
+  attack: function() {
+     console.log(`${this.fullName} сейчас с мячом и начинает атаку!`);
+  },
+  scoreGoal(sound) {
+     console.log(`${this.fullName} забил гол! Вот это да!`);
+     this.celebrate(sound);
+  },
+  celebrate(sound) {
+     console.log(sound);
+  },
+  goToSubstitution: function(newPlayer) {
+     console.log(`${this.fullName} уходит на замену. На поле выходит ${newPlayer}`);
+  }
+};
 
+const attack = footballer.attack.bind(footballer);
+attack();
 
+footballer.scoreGoal.call(footballer, 'Сиииии');
+footballer.goToSubstitution.apply(footballer, ['Paulo Dibala']);
+
+// это не функции
+// score('Сиииии');
+// substitute('Paulo Dibala');
 
 
 
