54d10c4398a553d4fe218cfeeb1da1c7e4414e6a
ZagSerSan <yan.steys1997@gmail.com>
2023-03-01 19:35:51 +0100

    Задание 2

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,12 +1,32 @@
 //TODO ========= 1й Модуль ==============
 
+const dog = {
+  name: 'Чарли',
+  type: 'Собака',
+  makeSound() {
+     return 'Гав-Гав';
+  }
+}
 
+const bird = {
+  name: 'Петя',
+  type: 'Воробей',
+  makeSound() {
+     return 'Чик-чирик';
+  }
+}
 
+function makeDomestic(isDomestic) {
+  this.isDomestic = isDomestic;
+  const sound = this.makeSound();
+  console.log(`${this.type} по имени ${this.name} говорит ${sound}`);
+  return this
+}
 
+console.log(makeDomestic.bind(dog, true)());
 
-
-
-
+console.log(makeDomestic.call(bird, false));
+console.log(makeDomestic.apply(bird, [false]));
 
 
 
