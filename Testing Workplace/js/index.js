47d41e774fdddd56174ed9bfd727abc09a9b736c
ZagSerSan <yan.steys1997@gmail.com>
2023-03-02 13:41:05 +0100

    13. Классы + ООП - #1

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,6 +1,27 @@
 //TODO ========= 1й Модуль ==============
 
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.technologies = [];
+    this.status = 'Junior';
+  }
+  setTechnologies (technologies) {
+    this.technologies = [
+      ...this.technologies,
+      ...technologies,
+    ];
+  }
+  setNewStatus (newStatus) {
+    this.status = newStatus;
+  }
+}
 
+const student = new Student ('Maxim', 20);
+student.setTechnologies([ 'HTML', 'CSS', 'JavaScript' ]);
+student.setNewStatus('Middle');
+console.log(student);
 
 
 
