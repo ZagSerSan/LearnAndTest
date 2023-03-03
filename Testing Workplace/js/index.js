2cbed504e3c4db8d8ab9a65e77803fd5f9587aea
ZagSerSan <yan.steys1997@gmail.com>
2023-03-03 15:01:39 +0100

    Encapsulation

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,5 +1,32 @@
 //TODO ========= 1й Модуль ==============
 
+class Developer {
+  #privateValue
+
+  constructor(name) {
+    this.name = name,
+    this.#privateValue = 'js'
+  }
+
+  get getPrivate() {
+    return this.#privateValue
+  }
+  set setPrivate(newValue) {
+    return this.#privateValue = newValue
+  }
+
+  sayHallo() {
+    console.log('Hello!');
+  }
+}
+
+const mainDeveloper = new Developer('Сергей', 'JavaScript')
+console.log(mainDeveloper);
+console.log(mainDeveloper.getPrivate);
+console.log(mainDeveloper.setPrivate = 'css');
+console.log('get:', mainDeveloper.getPrivate);
+
+
 
 
 
