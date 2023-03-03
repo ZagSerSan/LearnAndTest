9852b9060ff305b651c6d8db879d4d0f9cdab20a
ZagSerSan <yan.steys1997@gmail.com>
2023-03-03 08:17:12 +0100

    задание №4

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -54,6 +54,39 @@ class Dictionary {
   }
 }
 
+class HardWordsDictionary extends Dictionary {
+  constructor(name) {
+    super(name);
+  }
+  add(word, description) {
+    // проверка на наличие такого слова
+    if (this.keys.some(item => {return item === word})) {
+      console.log(`Слово '${word}' уже есть!`);
+    } else {
+      // добавление слова в words, если такого не было при проверке
+      this.words[word] = {
+        word: word,
+        description: description,
+        isDifficult: true
+      }
+      console.log(`Слово '${word}' добавлено как сложное.`);
+    }
+    // массив ключей words, для провекри наличия слова
+    this.keys = Object.keys(this.words);
+  }
+}
+
+const hardWordsDictionary = new HardWordsDictionary('Сложные слова');
+hardWordsDictionary.add('дилетант', 'Тот, кто занимается наукой или искусством без специальной подготовки, обладая только поверхностными знаниями.');
+hardWordsDictionary.add('неологизм', 'Новое слово или выражение, а также новое значение старого слова.');
+hardWordsDictionary.add('квант', 'Неделимая часть какой-либо величины в физике.'); 
+hardWordsDictionary.remove('неологизм');
+hardWordsDictionary.showAllWords();
+
+// console.log(hardWordsDictionary);
+
+/*
+
 const dictionary = new Dictionary('Толковый словарь');
 dictionary.add('JavaScript', 'популярный язык программирования');
 dictionary.add('Веб-разработчик', 'Человек, который создает новые сервисы и сайты или поддерживает и дополняет существующие');
@@ -62,6 +95,20 @@ dictionary.remove('JavaScript');
 dictionary.showAllWords();
 // Веб-разработчик - Человек, который создает новые сервисы и сайты или поддерживает // и дополняет существующие
 console.log(dictionary);
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /**
