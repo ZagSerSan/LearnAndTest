0ef85724f7aecc7f95d6a8fcaaf7e52834cb8671
ZagSerSan <yan.steys1997@gmail.com>
2023-03-07 11:47:53 +0100

    text.slice

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -7,6 +7,12 @@ mainNavButtons.forEach(button => {
       item.classList.remove('main-navigation__button-item_selected')
     });
     const {target} = event;
+    
+    let text = (target.textContent).trim();
+    text = text.slice(0, text.length-1)
+    console.log(text);
+    target.textContent = text;
+
     target.classList.add('main-navigation__button-item_selected');
 
   });
