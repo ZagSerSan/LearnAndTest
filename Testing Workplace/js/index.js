fd5cf161b00253e0b42132ef6eeffb4888ad28f8
ZagSerSan <yan.steys1997@gmail.com>
2023-03-07 11:26:47 +0100

    const {target} = event

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,9 +1,16 @@
 
+const mainNavButtons = document.querySelectorAll('.main-navigation__button-item');
 
+mainNavButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    mainNavButtons.forEach(item => {
+      item.classList.remove('main-navigation__button-item_selected')
+    });
+    const {target} = event;
+    target.classList.add('main-navigation__button-item_selected');
 
-
-
-
+  });
+});
 
 
 
