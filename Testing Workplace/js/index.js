1eb8607097ffe4fae7b56ad4cbc74dfbc20b918b
ZagSerSan <yan.steys1997@gmail.com>
2023-03-08 10:42:45 +0100

    Задание #2 (2 часть ДОМ) IS DONE!

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,3 +1,4 @@
+
 // массив задач
 const tasks = [
   {
@@ -16,9 +17,10 @@ const tasks = [
       text: 'Выполнить ДЗ после урока',
   },
 ];
-// получение блока задач
+// получение блока для добавления задач
 const tasksList = document.querySelector('.tasks-list');
-// создание шаблона айтема
+
+// функция для добавления задач из массива
 function createElementByArray(array) {
   array.forEach((item)=> {
      let taskItem = document.createElement('div');
@@ -60,27 +62,66 @@ form.addEventListener('submit', (event) => {
     completed: false,
     text: `${taskName.value}`,
   }
-  tasks.push(newTask);
-  console.log(tasks);
+  const itContain = tasks.some(item => {
+    return item.text === taskName.value;
+  })
+  // функция добавления элемента на страницу
+  function addTaskToHtml() {
+    tasks.push(newTask);
+    // создание и добавление элемента на страницу
+    let taskItem = document.createElement('div');
+    taskItem.className = 'task-item';
+    taskItem.dataset.taskId = `${newTask.id}`;
+    taskItem.innerHTML =
+    `<div class="task-item__main-container">
+    <div class="task-item__main-content">
+        <form class="checkbox-form">
+            <input class="checkbox-form__checkbox" type="checkbox" id="task-${newTask.id}">
+            <label for="task-${newTask.id}"></label>
+        </form>
+        <span class="task-item__text">
+            ${newTask.text}
+        </span>
+    </div>
+    <button class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
+        Удалить
+    </button>
+    </div>`
+  
+    tasksList.append(taskItem);
+    console.log(tasks);
+    return tasks
+  }
+  
+  const errorBlock = document.createElement('span')
+  errorBlock.className = 'error-message-block'
+  errorBlock.textContent = 'Задача с таким названием уже существует.'
+  let isError = document.querySelector('.error-message-block');
 
-  // создание и добавление элемента на страницу
-  let taskItem = document.createElement('div');
-  taskItem.className = 'task-item';
-  taskItem.dataset.taskId = `${newTask.id}`;
-  taskItem.innerHTML =
-  `<div class="task-item__main-container">
-  <div class="task-item__main-content">
-      <form class="checkbox-form">
-          <input class="checkbox-form__checkbox" type="checkbox" id="task-${newTask.id}">
-          <label for="task-${newTask.id}"></label>
-      </form>
-      <span class="task-item__text">
-          ${newTask.text}
-      </span>
-  </div>
-  <button class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
-      Удалить
-  </button>
-  </div>`
-  tasksList.append(taskItem);
+  // логика при наличии ошибки
+  if (isError) { // если есть ошибка
+    // если активирована ошибка и пустая строка
+    if (isError && taskName.value === '') {
+      isError.textContent = 'Название задачи не должно быть пустым.'
+    } else if (isError && taskName.value !== '' && !itContain) {
+    // если ошибка, пустая строка и такой задачи нету
+      isError.remove();
+      addTaskToHtml();
+    } else if (isError && taskName.value !== '' && itContain) {
+    // если ошибка, пустая строка и такая задача есть
+      isError.textContent = 'Задача с таким названием уже существует.'
+    }
+  // БЕЗ ОШИБКИ: если пустая строка 
+  } else if (taskName.value === '') {
+    console.log('пустая строка');
+    errorBlock.textContent = 'Название задачи не должно быть пустым.'
+    tasksList.append(errorBlock);
+  } else if (!itContain) {
+  // если такой задачи нету 
+    addTaskToHtml()
+  } else {
+  // если такая задача есть 
+    tasksList.append(errorBlock);
+  }
+
 });
