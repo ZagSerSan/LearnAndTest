f154c62639009f25d625a64dd77f8fa8c777fae0
ZagSerSan <yan.steys1997@gmail.com>
2023-03-15 18:43:15 +0100

    prep space

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,18 +1,19 @@
 
-const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
 
-const getTodosByIds = async (ids) => {
-  try {
-    console.log('Загрузка...');
-    const requests = await ids.map(id => fetch(`${TODOS_URL}/${id}`));
-    const responses = await Promise.all(requests)
-    const dataResults = responses.map(data => data.json());
-    const allTasks = await Promise.all(dataResults);
-    console.log('Результат:');
-    console.log('allTasks:', allTasks);
-  } catch { error => console.log('error', error);
-  } finally {
-    console.log('finally');
-  }
-}
-getTodosByIds([43, 21, 55, 100, 10]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
