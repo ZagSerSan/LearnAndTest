bc8755a9f98aaabe9a03cbf669f8f1ce20051a86
ZagSerSan <yan.steys1997@gmail.com>
2023-03-15 17:08:39 +0100

    #3 is done

--- Testing Workplace/js/index.js
+++ Testing Workplace/js/index.js
@@ -1,24 +1,22 @@
 
-/**html шаблон 
-<li class="photo-item">
-  <img class="photo-item__image" src="https://via.placeholder.com/600/92c952">
-  <h3 class="photo-item__title">
-    accusamus beatae ad facilis cum similique qui sunt
-  </h3>
-</li>
-*/
-
 // контейнер для вывода пользователей в html
 const dataContainerHTML = document.querySelector('#data-container');
 
 // функция создания элемента
-function creatUserElement(userName) {
+function creatUserElement(userPhotoObj) {
+  // li
   const userElement = document.createElement('li');
-  const userElementAnchor = document.createElement('a');
-  userElementAnchor.href = '#';
-  userElementAnchor.textContent = userName;
-
-  userElement.append(userElementAnchor);
+  // img
+  const userElement_image = document.createElement('img');
+  userElement_image.className = 'photo-item__image';
+  userElement_image.src = userPhotoObj.url;
+  // h3
+  const userElement_h3 = document.createElement('h3');
+  userElement_h3.className = 'photo-item__title';
+  userElement_h3.textContent = userPhotoObj.title;
+  
+  userElement.append(userElement_image);
+  userElement.append(userElement_h3);
   return userElement
 }
 
@@ -37,20 +35,20 @@ toggleLoaderHTML()
 
 //todo
 function getFastestLoadedPhoto(ids) {
-  const URL = 'https://jsonplaceholder.typicode.com/photos/1'
+  const URL = ''
 
-
-  Promise.all(arrayOfIds.map(item => fetch(`https://jsonplaceholder.typicode.com/users/${item}`)))
-    .then(responses => {
+  Promise.race(ids.map(id => fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)))
+    .then(response => {
       // обработка ошибки
-      let isOk = responses.every(response => response.ok === true)
-      if (!isOk) {
+      if (!response.ok) {
         throw new Error('Ошибка запроса!');
       }
-      return Promise.all(responses.map(response => response.json()));
-    }).then(users => users.forEach(userItem => {
-      dataContainerHTML.append(creatUserElement(userItem.name));
-    }))
+      return response.json();
+    })
+    .then(userPhotoObj => {
+      console.log('photo id:', userPhotoObj.id);
+      dataContainerHTML.append(creatUserElement(userPhotoObj))
+    })
     .catch(error => {
       console.error(error);
     })
@@ -59,4 +57,5 @@ function getFastestLoadedPhoto(ids) {
     })
 }
 
-// getUsersByIds([5, 6, 2, 1])
+getFastestLoadedPhoto([60, 12, 55]);
+
