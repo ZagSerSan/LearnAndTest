2fd312ca994eabb3e1f16a6ddcfc405c45b4976d
ZagSerSan <yan.steys1997@gmail.com>
2023-03-20 16:03:00 +0100

    20. ES6 import export - js time from video lessons

--- Testing_workplace/index.js
+++ Testing_workplace/index.js
@@ -1,3 +1,8 @@
 import './index.css';
+// import { App as AppComponent } from './src/modules/app'
+import App from './src/modules/app'
 
 console.log('__work__');
+
+const app = new App();
+app.run();
