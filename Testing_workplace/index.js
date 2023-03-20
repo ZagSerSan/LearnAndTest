eb9c434199da1b302cb52bfa7801bfd0b3b3ec6f
ZagSerSan <yan.steys1997@gmail.com>
2023-03-20 17:54:46 +0100

    задание №2

--- Testing_workplace/index.js
+++ Testing_workplace/index.js
@@ -1,1 +1,16 @@
 import './index.css';
+
+// localStorage.setItem('isNotFirstVisit', 'true')
+const cookieButton = document.querySelector('.cookie-consent__button');
+const cookieConsent = document.querySelector('.cookie-consent');
+
+if (localStorage.getItem('isNotFirstVisit')) {
+   cookieConsent.style.display = 'none';
+}
+
+cookieButton.addEventListener('click', (event) => {
+   event.preventDefault();
+
+   cookieConsent.style.display = 'none';
+   localStorage.setItem('isNotFirstVisit', 'true')
+});
