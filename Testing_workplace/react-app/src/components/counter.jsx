8c7ffd661d06e63d06684889ef7fd25ecdcf84b5
ZagSerSan <yan.steys1997@gmail.com>
2023-04-04 15:30:23 +0200

    custom thing

--- Testing_workplace/react-app/src/components/counter.jsx
+++ Testing_workplace/react-app/src/components/counter.jsx
@@ -1,21 +1,36 @@
-import React from "react"
+import React,{useState} from "react"
 
 const Counter = () => {
-  let counter = 1
+  const [count, setCount] = useState(0)
 
   const formatCount = () => {
-    return counter === 0 ? 'empty' : counter
+    return count === 0 ? 'empty' : count
   }
   const getBageClasses = () => {
     let classes = 'badge m-2 '
-    classes += counter === 0 ? 'text-bg-warning' : 'text-bg-primary'
+    classes += count === 0 ? 'text-bg-warning' : 'text-bg-primary'
     return classes
+  }
+
+  function handleIncrement() {
+    count === 10 ? console.log('count не может быть больше 10') : setCount( (prevState) => prevState + 1)
+    // setCount( (prevState) => prevState + 1)
+  }
+  function handleDecrement() {
+    count === 0 ? console.log('count не может быть меньше нуля') : setCount( (prevState) => prevState - 1)
   }
 
   return (
     <>
         <span className={getBageClasses()}>{formatCount()}</span>
-        <button className="badge btn btn-primary btn-sm m-1">+</button>
+        <button 
+          className="badge btn btn-primary btn-sm m-1"
+          onClick={handleIncrement}
+        >+</button>
+        <button 
+          className="badge btn btn-primary btn-sm m-1"
+          onClick={handleDecrement}
+        >-</button>
     </>
   );
 }
