83eea0d0e849ccac92a34bd09d8c1e93049b11d8
ZagSerSan <yan.steys1997@gmail.com>
2023-04-04 09:11:29 +0200

    React.Fragment

--- Testing_workplace/react-app/src/components/counter.jsx
+++ Testing_workplace/react-app/src/components/counter.jsx
@@ -1,6 +1,11 @@
 import React from "react"
 
 const Counter = () => {
-   return <h2>It`s Counter Component!</h2>
+   return (
+      <>
+         <h2>It`s Counter Component!</h2>
+         <button>+</button>
+      </>
+   );
 }
 export default Counter
