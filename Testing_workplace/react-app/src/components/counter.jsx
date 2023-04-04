039b4edddf9b68613a601982d59de2f562df37eb
ZagSerSan <yan.steys1997@gmail.com>
2023-04-04 15:49:57 +0200

    9. Рендер списка из массива

--- Testing_workplace/react-app/src/components/counter.jsx
+++ Testing_workplace/react-app/src/components/counter.jsx
@@ -2,6 +2,7 @@ import React,{useState} from "react"
 
 const Counter = () => {
   const [count, setCount] = useState(0)
+  const [tags, setTag] = useState(['tag1', 'tag2', 'tag3'])
 
   const formatCount = () => {
     return count === 0 ? 'empty' : count
@@ -19,18 +20,29 @@ const Counter = () => {
   function handleDecrement() {
     count === 0 ? console.log('count не может быть меньше нуля') : setCount( (prevState) => prevState - 1)
   }
+  const handleTagChange = () => {
+    setTag(['tag4', 'tag5'])
+    // console.log('тут');
+  }
 
   return (
     <>
-        <span className={getBageClasses()}>{formatCount()}</span>
-        <button 
-          className="badge btn btn-primary btn-sm m-1"
-          onClick={handleIncrement}
-        >+</button>
-        <button 
-          className="badge btn btn-primary btn-sm m-1"
-          onClick={handleDecrement}
-        >-</button>
+      <ul>{
+            tags.map(tag => (
+              <li key={tag} 
+                className="btn btn-primary btn-sm m-1" onClick={handleTagChange}>{tag}</li>)
+            )
+          }
+      </ul>
+      <span className={getBageClasses()}>{formatCount()}</span>
+      <button 
+        className="badge btn btn-primary btn-sm m-1"
+        onClick={handleIncrement}
+      >+</button>
+      <button 
+        className="badge btn btn-primary btn-sm m-1"
+        onClick={handleDecrement}
+      >-</button>
     </>
   );
 }
