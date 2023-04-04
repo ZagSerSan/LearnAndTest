68f85eab514e865e850c43266af774f59b042484
ZagSerSan <yan.steys1997@gmail.com>
2023-04-04 11:34:22 +0200

    corrected code

--- Testing_workplace/react-app/src/components/counter.jsx
+++ Testing_workplace/react-app/src/components/counter.jsx
@@ -3,14 +3,20 @@ import React from "react"
 const Counter = () => {
   let counter = 1
 
-  
+  const formatCount = () => {
+    return counter === 0 ? 'empty' : counter
+  }
+  const getBageClasses = () => {
+    let classes = 'badge m-2 '
+    classes += counter === 0 ? 'text-bg-warning' : 'text-bg-primary'
+    return classes
+  }
 
-   return (
-      <>
-         <h1>{counter}</h1>
-         <button id="addCount" className="badge btn btn-primary btn-sm m-1">+{editCount()}</button>
-         <button id="remCount" className="badge btn btn-primary btn-sm m-1">-{editCount()}</button>
-      </>
-   );
+  return (
+    <>
+        <span className={getBageClasses()}>{formatCount()}</span>
+        <button className="badge btn btn-primary btn-sm m-1">+</button>
+    </>
+  );
 }
 export default Counter
