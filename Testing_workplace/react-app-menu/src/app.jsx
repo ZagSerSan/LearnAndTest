6f4fd9c3334fb7fa09ecd928a014281eaa944519
ZagSerSan <yan.steys1997@gmail.com>
2023-04-10 19:54:48 +0200

    prep project for example: Menu

--- Testing_workplace/react-app-menu/src/app.jsx
+++ Testing_workplace/react-app-menu/src/app.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 // components
-import Counter from './components/counter'
 import Page from './components/page'
 
 const App = () => {
-   return <Counter/>
+   return <Page/>
 }
 
 export default App
