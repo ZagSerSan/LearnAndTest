62a6a6a9d4c5d71c42b42e932b90062d4fe05136
ZagSerSan <yan.steys1997@gmail.com>
2023-04-11 18:18:38 +0200

    todo handleDelete

--- Testing_workplace/react-app/src/components/counter.jsx
+++ Testing_workplace/react-app/src/components/counter.jsx
@@ -1,34 +1,42 @@
 import React,{useState} from "react"
 
-const Counter = () => {
-  const [count, setCount] = useState(0)
+const Counter = (props) => {
+  const [value, setValue] = useState(props.value)
 
-  const formatCount = () => {
-    return count === 0 ? 'empty' : count
+  const formatValue = () => {
+    return value === 0 ? 'empty' : value
   }
   const getBageClasses = () => {
     let classes = 'badge m-2 '
-    classes += count === 0 ? 'text-bg-warning' : 'text-bg-primary'
-    return classes
+    return classes += (value === 0 ? 'text-bg-warning' : 'text-bg-primary')
   }
   function handleIncrement() {
-    count === 10 ? console.log('count не может быть больше 10') : setCount( (prevState) => prevState + 1)
+    value === 10 ? console.log('value не может быть больше 10') : setValue( (prevState) => prevState + 1)
   }
   function handleDecrement() {
-    count === 0 ? console.log('count не может быть меньше нуля') : setCount( (prevState) => prevState - 1)
+    value === 0 ? console.log('value не может быть меньше нуля') : setValue( (prevState) => prevState - 1)
   }
  
+  const handleDelete = () => {
+    console.log('handleDelete')
+ }
   return (
     <div>
-      <span className={getBageClasses()}>{formatCount()}</span>
+      <span>{props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
       <button 
         className="badge btn btn-primary btn-sm m-1"
         onClick={handleIncrement}
-      >+</button>
+      >
+        +
+      </button>
       <button 
         className="badge btn btn-primary btn-sm m-1"
         onClick={handleDecrement}
-      >-</button>
+      >
+        -
+      </button>
+      <button className="btn btn-danger btn-sm m-1" onClick={props.onDelete}>delete</button>
     </div>
   )
 }
--- Testing_workplace/react-app/src/components/countersList.jsx
+++ Testing_workplace/react-app/src/components/countersList.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import Counter from "./counter";
 
 const CountersList = () => {
-   const counters = [
-      {id: 0, value: 0},
-      {id: 1, value: 4},
-      {id: 2, value: 0}
-   ]
+   const [counters, setCounters] = useState([
+      {id: 0, value: 0, name: 'Ненужная вещь'},
+      {id: 1, value: 4, name: 'Ложка'},
+      {id: 2, value: 0, name: 'Вилка'},
+      {id: 3, value: 0, name: 'Тарелка'},
+      {id: 4, value: 0, name: 'Набор минималиста'}
+   ])
    
+   // const handleDelete = (id) => {
+   //    setCounters(prevState => 
+   //       prevState.filter(item => item._id !== id)
+   //    )
+   // }
+
    return <>
       {counters.map(count => 
-         <Counter key={count.id}/>
+         <Counter
+            key={count.id}
+            value={count.value}
+            name={count.name}
+            onDelete={handleDelete}
+         />
       )}
    </>
 }
