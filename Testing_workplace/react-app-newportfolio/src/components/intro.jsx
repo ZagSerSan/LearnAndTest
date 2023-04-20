aea1909562b8a3c8f0246066f5d545d99acaa4fd
ZagSerSan <yan.steys1997@gmail.com>
2023-04-20 16:09:43 +0200

    add component 'Social'

--- Testing_workplace/react-app-newportfolio/src/components/intro.jsx
+++ Testing_workplace/react-app-newportfolio/src/components/intro.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PHOTO from "../img/intro/photo.png"
+import Social from './social';
 
 const Intro = () => {
   return (
@@ -6,7 +8,17 @@ const Intro = () => {
       <div className="container">
         <div className="intro__inner">
 
-          <h1>intro</h1>
+        <div className="intro-content">
+            <div>
+              <h2 className="intro-content__greeting">Hello, I’m</h2>
+              <h1 className="intro-content__name">Serhii Zahorskyi</h1>
+              <p className="intro-content__prof">Frontend developer</p>
+              <Social/>
+            </div>
+         </div>
+         <div className="intro-photo">
+            <img src={PHOTO} alt="photo"/>
+         </div>
 
         </div>
       </div>
