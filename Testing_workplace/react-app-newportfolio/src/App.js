6b0af699a81643822235e10c7aa0f21de84a76bd
ZagSerSan <yan.steys1997@gmail.com>
2023-04-20 15:42:29 +0200

    create new component 'Intro'

--- Testing_workplace/react-app-newportfolio/src/App.js
+++ Testing_workplace/react-app-newportfolio/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import Header from './components/header';
+import Intro from './components/intro';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Intro/>
     </div>
   );
 }
---
+++ Testing_workplace/react-app-newportfolio/src/components/intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="container">
+        <div className="intro__inner">
+
+          <h1>intro</h1>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+ 
+export default Intro
