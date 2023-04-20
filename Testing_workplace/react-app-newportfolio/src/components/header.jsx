6277bfd639bc646a100cb73dfe9d2bf98705e543
ZagSerSan <yan.steys1997@gmail.com>
2023-04-20 15:34:10 +0200

    merge features/header

--- Testing_workplace/react-app-newportfolio/src/components/header.jsx
+++ Testing_workplace/react-app-newportfolio/src/components/header.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React from 'react'
 import AVATAR from '../img/header/avatarSmall.png'
 
 const Header = () => {
   return (
     <header className='header'>
       <div  className='container'>
+
         <div className='header__inner'>
           <div className='header-avatar'>
             <img className='header-avatar__img' src={AVATAR} alt="avatar" />
-            <div className="header-avatar-info">
-              <h1>Serhii Zagorskyi</h1>
-              <p>Frontend developer</p>
+            <div className="header-avatar__content">
+              <h1 className='header-avatar__name'>Serhii<br/>Zagorskyi</h1>
+              <p className='header-avatar__prof'>Frontend dev</p>
             </div>
           </div>
+
           <nav className='header-nav'>
-            <a className='header-nav__link' href="#">nav-link</a>
-            <a className='header-nav__link' href="#">nav-link</a>
+            <a className='header-nav__link' href="#">Portfolio</a>
+            <a className='header-nav__link' href="#">About me</a>
           </nav>
         </div>
+
       </div>
-    </header> 
-   );
+    </header>
+   )
 }
  
-export default Header;
+export default Header
