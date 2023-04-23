2ac6f5f6bd0d45452d81f10893a815f467d9387a
ZagSerSan <yan.steys1997@gmail.com>
2023-04-23 14:51:04 +0200

    added sliders for all modals windows

--- Testing_workplace/react-app-newportfolio/src/components/modalSlider.jsx
+++ Testing_workplace/react-app-newportfolio/src/components/modalSlider.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import Slider from "react-slick"
 
-// activebox
+//1 activebox
 import ActiveboxSlide_1 from "../img/works/1-activebox/slide_1.jpg"
 import ActiveboxSlide_2 from "../img/works/1-activebox/slide_2.jpg"
 import ActiveboxSlide_3 from "../img/works/1-activebox/slide_3.jpg"
 import ActiveboxSlide_4 from "../img/works/1-activebox/slide_4.jpg"
-// mogo
+//2 mogo
 import MogoSlide_1 from "../img/works/2-mogo/slide-1.png"
 import MogoSlide_2 from "../img/works/2-mogo/slide-2.png"
 import MogoSlide_3 from "../img/works/2-mogo/slide-3.png"
 import MogoSlide_4 from "../img/works/2-mogo/slide-4.png"
 import MogoSlide_5 from "../img/works/2-mogo/slide-5.png"
 import MogoSlide_6 from "../img/works/2-mogo/slide-6.png"
+//3 designer-portfolio
+import DesPortfSlide_1 from "../img/works/3-designer-portfolio/slide-1.png"
+import DesPortfSlide_2 from "../img/works/3-designer-portfolio/slide-2.png"
+import DesPortfSlide_3 from "../img/works/3-designer-portfolio/slide-3.png"
+import DesPortfSlide_4 from "../img/works/3-designer-portfolio/slide-4.png"
+//4 calculator
+import calcPreview from "../img/works/4-calculator/preview.png"
+//5 todo list
+import todoListPreview from "../img/works/5-TodoList/slider1.png"
 
 const ModalSlider = ({currentModal}) => {
   // slider settings
@@ -23,10 +32,6 @@ const ModalSlider = ({currentModal}) => {
     slidesToShow: 1,
     slidesToScroll: 1
   }
-
-  //todo массив слайдов.
-  // добавить слайды остальных проектов
-
   const slidersArray = [
     {
       modalName: 'activebox',
@@ -35,6 +40,18 @@ const ModalSlider = ({currentModal}) => {
     {
       modalName: 'mogo',
       modalSlides: [MogoSlide_1, MogoSlide_2, MogoSlide_3, MogoSlide_4, MogoSlide_5, MogoSlide_6]
+    },
+    {
+      modalName: 'designerPortfolio',
+      modalSlides: [DesPortfSlide_1, DesPortfSlide_2, DesPortfSlide_3, DesPortfSlide_4]
+    },
+    {
+      modalName: 'customCalculator',
+      modalSlides: [calcPreview]
+    },
+    {
+      modalName: 'todolist',
+      modalSlides: [todoListPreview]
     }
   ]
   // новый массив отобранных слайдов для текущего окна
